b1d3fc23b6c09df2b708405b0e283a0dbb7f69a6
MinhKhoiUET <minhkhoi281103@gmail.com>
2024-08-14 17:33:22 +0700

    complete homepage UI

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -13,6 +13,9 @@ import {
 import { Control } from 'react-hook-form'
 import { FormFieldType } from './forms/PatientForm'
 import Image from 'next/image'
+import PhoneInput from 'react-phone-number-input'
+import 'react-phone-number-input/style.css'
+import { E164Number } from 'libphonenumber-js';
 
 interface CustomProps{
     control: Control<any>,
@@ -53,6 +56,20 @@ const RenderField = ({field, props} : {field:any; props: CustomProps}) => {
                         />
                     </FormControl>
                 </div>
+            )
+        case FormFieldType.PHONE_INPUT:
+            return(
+                <FormControl>
+                    <PhoneInput
+                        defaultCountry = "VN"
+                        placeholder = {placeholder}
+                        international
+                        withCountryCallingCode
+                        value = {field.value as E164Number | undefined}
+                        onChange={field.onChange}
+                        className='input-phone'
+                    />
+                </FormControl>
             )
     }
 }
--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,9 +1,9 @@
-"use client"
- 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { Button } from "@/components/ui/button"
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -12,68 +12,103 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import CustomFormField from "../CustomFormField"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import CustomFormField from "../CustomFormField";
+import SubmitButton from "./../SubmitButton";
+import { useState } from "react";
+import { UserFormValidation } from "@/lib/validation";
+import { useRouter } from "next/navigation";
 
 export enum FormFieldType {
-  INPUT = 'input',
-  TEXTAREA = 'textarea',
-  PHONE_INPUT = 'phoneinput',
-  CHECKBOX = 'checkbox',
-  DATE_PICKER = 'datePicker',
-  SELECT = 'select',
-  SKELETON = 'skeleton'
-
+  INPUT = "input",
+  TEXTAREA = "textarea",
+  PHONE_INPUT = "phoneinput",
+  CHECKBOX = "checkbox",
+  DATE_PICKER = "datePicker",
+  SELECT = "select",
+  SKELETON = "skeleton",
 }
- 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
- 
+
 const PatientForm = () => {
+  const router = useRouter();
+
+  const [isLoading, setIsLoading] = useState(false);
+
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      phone: "",
     },
-  })
- 
+  });
+
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(
+    {name,
+    email,
+    phone}: z.infer<typeof UserFormValidation>
+  ) {
+    setIsLoading(true);
+    try {
+      // const userData = {
+      //   name,
+      //   email,
+      //   phone,
+      // };
 
-    console.log(values)
+      // const user = await createUser(userDate)
+
+      // if(user) router.push(`/patient/${user.$id}/register`)
+
+    } catch (error) {
+      console.log(error);
+    }
   }
-  return(
+  return (
     <Form {...form}>
-    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-      <section className="mb-12 space-y-4">
-        <h1 className="header">
-          Hi there 👋
-        </h1>
-        <p className="text-dark-700">Schedule your first appointment.</p>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Hi there 👋</h1>
+          <p className="text-dark-700">Schedule your first appointment.</p>
+        </section>
 
-      </section>
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="name"
+          label="Full name"
+          placeholder="Minh Khoi"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
+        />
 
-      <CustomFormField 
-        fieldType={FormFieldType.INPUT}
-        control={form.control}
-        name = "name"
-        label = "Full name"
-        placeholder = "Minh Khoi"
-        iconSrc = "/assets/icons/user.svg"
-        iconAlt = "user"
-      />
-      
-      <Button type="submit">
-        Submit
-      </Button>
-    </form>
-  </Form>
-  )
-}
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="email"
+          label="Email"
+          placeholder="example@example.com"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="email"
+        />
 
-export default PatientForm
+        <CustomFormField
+          fieldType={FormFieldType.PHONE_INPUT}
+          control={form.control}
+          name="phone"
+          label="Phone number"
+          placeholder="0123456789"
+          iconSrc="/assets/icons/phone.svg"
+          iconAlt="email"
+        />
+
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
+      </form>
+    </Form>
+  );
+};
+
+export default PatientForm;
